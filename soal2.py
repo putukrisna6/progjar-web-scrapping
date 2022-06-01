b72d4b9e3a8eb8285254c658a965aae46ca8757a
daffainfo <md15ev@gmail.com>
2022-06-01 20:35:59 +0700

    Fix soal 2

--- soal2.py
+++ soal2.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 def getGoPackage(query, n):
     page = requests.get("https://pkg.go.dev/search?limit=" + str(n) + "&m=package&q=" + query)
     soup = BeautifulSoup(page.content, 'html.parser')
-    pkg_detail = soup.find("div", class_="SearchSnippet")
-    print(pkg_detail)
-    # link = pkg_detail.select('a', href=True)
-    # print(link)
-    # return desc
-    # return num + ", akan mengambil: " + title
+    for list_pkg in soup.findAll("div", class_="SearchSnippet"):
+        print("https://pkg.go.dev" + list_pkg.find('a')['href'])
+        if (list_pkg.find('p') == None):
+            print("No description\n")
+        else:
+            print(list_pkg.find('p').text.strip()+"\n")
 
 getGoPackage("sort", 10)
 # print(getGoBlog(sys.argv[1]))
